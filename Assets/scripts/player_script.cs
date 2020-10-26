8efa237fc750a132c539d5e791e7c9921221546f
Taylor Torres <ta850983@ucf.edu>
2020-10-26 08:04:26 -0400

    first commit

--- Assets/scripts/player_script.cs
+++ Assets/scripts/player_script.cs
@@ -6,20 +6,33 @@ using UnityEngine.UI;
 public class player_script : MonoBehaviour
 {
     private Rigidbody2D rd2d;
-
     public float speed;
-
     public Text score;
-
+    public Text winText;
     private int scoreValue = 0;
+
+    public Text livesText;
+    private int lives;
+
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    public AudioSource musicSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        winText.text = "";
+        lives = 3; 
+        SetLivestext ();
+        
+        musicSource.clip = musicClipOne;
+        musicSource.loop = true;
+        musicSource.Play();
+        
     }
-
+    
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -35,7 +48,29 @@ public class player_script : MonoBehaviour
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            if (scoreValue == 4)
+            {
+                lives = 3; 
+                SetLivestext ();
+                transform.position = new Vector2(40.3f, 1.55f);
+            }
+            if (scoreValue >= 8) 
+            {
+                winText.text = "You Win! Game Created By Taylor Torres";
+                musicSource.Stop();
+                musicSource.loop = false;
+                musicSource.clip = musicClipTwo;
+                musicSource.Play();
+                //Destroy(this);
+            }
+           
+        } else if (collision.collider.tag == "enemy_tag")
+        {
+            Destroy(collision.collider.gameObject);
+            lives -= 1;
+            SetLivestext ();
         }
+
 
     }
 
@@ -49,4 +84,18 @@ public class player_script : MonoBehaviour
             }
         }
     }
+
+   
+    void SetLivestext () 
+    {
+        livesText.text = "Lives: " + lives.ToString ();
+        if (lives <= 0)
+        {
+            winText.text = "You Lose! Game by Taylor Torres";
+
+            Destroy(this);
+        } 
+    }
+    
+
 }
